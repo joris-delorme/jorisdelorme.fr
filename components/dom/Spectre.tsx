4c6ac94e8e117373ccb7d527aaf2b9e627965699
joris-delorme <joris.delorme@epitech.digital>
2023-08-31 15:06:56 +0200

    favicon

--- components/dom/Spectre.tsx
+++ components/dom/Spectre.tsx
@@ -1,6 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import { Suspense, useLayoutEffect } from 'react'
 
 const Spectre = dynamic(() => import('@/components/canvas/Spectre').then((mod) => mod.Spectre), { ssr: false })
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
@@ -8,6 +8,13 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
 })
 
 export function SpectreDOM() {
+
+  useLayoutEffect(() => {
+    const icon = document.querySelector("link[rel='icon']") as HTMLLinkElement    
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => icon.href = e.matches ? "/favicon.ico" : "/dark/favicon.ico")
+    icon.href = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? "/favicon.ico" : "/dark/favicon.ico"
+  }, [])
+
   return (
     <>
       {/*@ts-ignore*/}
