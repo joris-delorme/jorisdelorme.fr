f7ed187653c88bd3a2406822242d10f1a87f21c4
joris-delorme <joris.delorme@epitech.digital>
2024-09-10 14:56:55 +0200

    SEO

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  name: "Joris Delorme",
+  name: "Joris Delorme | Creative Developer & Entrepreneur",
   description:
-    "My name is Joris Delorme a creative developer and entrepreneur. I craft",
+    "My name is Joris Delorme, a creative developer and entrepreneur who likes software and beautiful things. I craft dreams with code.",
   url: "https://jorisdelorme.fr",
   ogImage: "https://jorisdelorme.fr/og.jpg",
   links: {
